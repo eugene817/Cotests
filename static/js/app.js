a2be45643e0b67aa1f363bf4dcc72686569cda88
Yauheni Luferchyk <luferchikz@gmail.com>
2026-09-04 20:32:39 +0200

    phase 2 begin

--- static/js/app.js
+++ static/js/app.js
@@ -1,10 +1,16 @@
-document.body.addEventListener("htmx:configRequest", (event) => {
+document.addEventListener("htmx:configRequest", (event) => {
   const csrfToken = document.cookie.split("; ").find((cookie) => cookie.startsWith("csrf_token="))?.split("=")[1]
   if (csrfToken) event.detail.headers["X-CSRF-Token"] = decodeURIComponent(csrfToken)
 })
 
-document.body.addEventListener("htmx:beforeSwap", (event) => {
-  if ([400, 401, 403, 409].includes(event.detail.xhr.status)) {
+document.addEventListener("htmx:beforeRequest", (event) => {
+  const form = event.detail.elt.closest("form")
+  const target = form?.getAttribute("hx-target")
+  if (target?.includes("error")) document.querySelector(target)?.replaceChildren()
+})
+
+document.addEventListener("htmx:beforeSwap", (event) => {
+  if ([400, 401, 403, 404, 409, 500].includes(event.detail.xhr.status)) {
     event.detail.shouldSwap = true
     event.detail.isError = false
   }
